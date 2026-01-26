c84fff66cbe3c5ad32dcc92bca5ef53e50ba1cd4
Roman Metlinskyi <gmail@metlinskyi.com>
2026-01-26 21:31:02 +0100

    unit tests of endpoints

---
+++ tests/UnitTests/Routes/SettingsEndpointTests.cs
@@ -0,0 +1,23 @@
+﻿using Api.Routes.Web;
+
+namespace UnitTests.Routes;
+
+public class SettingsEndpointTests : UnitTestFor<SettingsEndpoint>
+{
+    protected override SettingsEndpoint CreateService()
+    {
+        return new SettingsEndpoint(null);
+    }
+
+    [Test]
+    public async Task Handler_ReturnsSettingsResponse()
+    {
+        // Arrange
+
+        // Act
+        var response = await SUT.Handler();
+
+        // Assert
+        That(response, Is.InstanceOf<SettingsResponse>());
+    }
+}
